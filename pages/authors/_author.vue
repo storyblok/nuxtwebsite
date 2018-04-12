3d0979a818a4555b3985723d627cc8e6bf39398c
Dominik Angerer <dominikangerer1@gmail.com>
2018-04-12 12:58:18 +0200

    adds author detail page

---
+++ pages/authors/_author.vue
@@ -0,0 +1,33 @@
+<template>
+  <article v-editable="author.content">
+    <img :src="resizeImages(author.content.image, '100x0')" :alt="author.content.image_alt">
+    <h1>{{author.content.firstname}} {{author.content.lastname}}</h1>
+    <ul>
+      <li :key="social_link._uid" v-for="social_link in author.content.socials">
+        <a :href="social_link.link.cached_url" target="_blank" rel="noopener nofollow">{{social_link.type}}</a>
+      </li>
+    </ul>
+  </article>
+</template>
+
+<script>
+import { isEditMode, resizeImages } from '@/plugins/helper'
+import MarkdownContent from '@/components/MarkdownContent'
+
+export default {
+  mounted () {
+    isEditMode(this)
+  },
+  components: {
+    MarkdownContent
+  },
+  methods: {
+    resizeImages
+  },
+  async asyncData (context) {
+    // load the content-entry at the current path - will be something like: '/posts/first-post'
+    const { data } = await context.app.$storyapi.get(`cdn/stories${context.route.path}`, { version: 'draft' })
+    return { author: data.story }
+  }
+}
+</script>
