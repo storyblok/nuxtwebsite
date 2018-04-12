33503729445e30da2a78d1ce26f1c978ed39a8c5
Dominik Angerer <dominikangerer1@gmail.com>
2018-04-12 13:39:14 +0200

    adds authors to post via resolve_links

--- pages/posts/_post.vue
+++ pages/posts/_post.vue
@@ -1,8 +1,12 @@
 <template>
-  <article v-editable="story.content">
-    <img :src="resizeImages(story.content.image, '600x0')" :alt="story.content.image_alt">
-    <h1>{{story.name}}</h1>
-    <MarkdownContent :content="story.content.content" />
+  <article v-editable="post.content">
+    <img :src="resizeImages(post.content.image, '600x0')" :alt="post.content.image_alt">
+    <h1>{{post.name}}</h1>
+    <div>
+      <img :src="author.content.image" :alt="author.content.firstname + ' ' + author.content.lastname">
+      <span>{{author.content.firstname}} {{author.content.lastname}}</span>
+    </div>
+    <MarkdownContent :content="post.content.content" />
   </article>
 </template>
 
@@ -22,8 +26,8 @@ export default {
   },
   async asyncData (context) {
     // load the content-entry at the current path - will be something like: '/posts/first-post'
-    const { data } = await context.app.$storyapi.get(`cdn/stories${context.route.path}`, { version: 'draft' })
-    return { story: data.story }
+    const { data } = await context.app.$storyapi.get(`cdn/stories${context.route.path}`, { version: 'draft', resolve_links:1 })
+    return { post: data.story, author: data.story.content.author.story }
   }
 }
 </script>
