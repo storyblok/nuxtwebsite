c4c82c82ed9c393acd106690ca37bef686451184
Dominik Angerer <dominikangerer1@gmail.com>
2018-04-12 08:44:15 +0200

    adds editable and HTMLContent

--- pages/posts/_post.vue
+++ pages/posts/_post.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
+  <article v-editable="story.content">
     <h1>{{story.name}}</h1>
-    <HTMLContent :content="blok.content" />
-  </div>
+
+    <HTMLContent :content="story.content.content" />
+  </article>
 </template>
 
 <script>
 import { isEditMode } from '@/plugins/is-edit-mode'
+import HTMLContent from '@/components/HTMLContent'
 
 export default {
   mounted () {
     isEditMode(this)
+  },
+  components: {
+    HTMLContent
   },
   async asyncData (context) {
     const full_slug = `posts/${context.params.post}`
