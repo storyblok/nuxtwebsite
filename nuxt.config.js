7b7508a9927afd609dcf7d62f60cbb1abf8ed275
Dominik Angerer <dominikangerer1@gmail.com>
2018-04-12 07:55:16 +0200

    adds content-types and components plugins to nuxt-config

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,10 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'A simple Nuxt.js setup to create websites with blog feature using Storyblok as CMS and Netlify to deploy it.' }
     ],
+    plugins: [
+      '@/plugins/content-types',
+      '@/plugins/components'
+    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
