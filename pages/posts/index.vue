02d18b74b568862a33d08e7e80b201dd086d7bd0
Dominik Angerer <dominikangerer1@gmail.com>
2018-04-12 09:04:56 +0200

    basic post overview

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    Post Overview
+    <h1>Posts</h1>
+    <article :key="post.id" v-for="post in posts">
+      <nuxt-link :to="'/' + post.full_slug">
+        <img :src="resizeImages(post.content.image, '300x0')" :alt="post.content.image_alt">
+        <h2>{{post.name}}</h2>
+      </nuxt-link>
+    </article>
   </div>
 </template>
 
 <script>
-import { isEditMode } from '@/plugins/helper'
+import { isEditMode, resizeImages } from '@/plugins/helper'
 
 export default {
   mounted () {
     isEditMode(this)
+  },
+  methods: {
+    resizeImages
   },
   async asyncData (context) {
     const { data } = await context.app.$storyapi.get(`cdn/stories/`, { version: 'draft', starts_with: `posts/` })
